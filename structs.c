f8591a81e66f72b72dac132fd1a0620cf8212af2
Milan Rufini de Andrade <36967571+drakcamicazi@users.noreply.github.com>
2019-11-13 10:08:19 -0300

    Structs.c agora guardam todos os meses
    Falta testar

--- structs.c
+++ structs.c
@@ -16,15 +16,25 @@ typedef struct{
 } Operacao;
 
 int main(){
-	FILE *mes1;
-  int i = 0, j  = 0;
+	FILE *mes1, *mes2, *mes3, *mes4, *mes5;
+  int i, j, k;
   char linha[11];
-  Operacao o[100];
+  Operacao o[16600]; //ao total tem-se 16600 operacoes em todos os arquivos
 
   mes1 = fopen("../mes_1.txt", "r");
   verificarAbertura(mes1);
+  mes2 = fopen("../mes_2.txt", "r");
+  verificarAbertura(mes2);
+  mes3 = fopen("../mes_3.txt", "r");
+  verificarAbertura(mes3);
+  mes4 = fopen("../mes_4.txt", "r");
+  verificarAbertura(mes4);
+  mes5 = fopen("../mes_5.txt", "r");
+  verificarAbertura(mes5);
 
   //o i e o j são necessários pra separar uma "linha válida" de uma "vazia", que o fgets tava achando
+  i = 0;
+  j = 0;
   while (fgets(linha, 11, mes1)){
     if (i % 2 == 0){
       printf("%i, %i: %s \n", i, j, linha); //para acompanhar os índices de i e j. pode ser apagado depois
@@ -34,11 +44,69 @@ int main(){
     }
     i++;
   }
+  printf("Fim do primeiro arquivo.\n");
 
   i = 0;
-  while (i < 100) {
+  j = 0;
+  while (fgets(linha, 11, mes2)){
+    if (i % 2 == 0){
+      k = j + 100;
+      printf("%i, %i: %s \n", i, k, linha); //+100 pois ja tem 100 registros no struct
+      strcpy(o[k].cod, strtok(linha, "\n")); 
+      o[k].mes = 2;
+      j++;
+    }
+    i++;
+  }
+  printf("Fim do segundo arquivo.\n");
+  
+  i = 0;
+  j = 0;
+  while (fgets(linha, 11, mes3)){
+    if (i % 2 == 0){
+      k = j + 100 + 500;
+      printf("%i, %i: %s \n", i, k, linha); //+100 + 500 pois ja tem 600 registros no struct
+      strcpy(o[k].cod, strtok(linha, "\n")); 
+      o[k].mes = 3;
+      j++;
+    }
+    i++;
+  }
+  printf("Fim do terceiro arquivo.\n");
+  
+  i = 0;
+  j = 0;
+  while (fgets(linha, 11, mes4)){
+    if (i % 2 == 0){
+      k = j + 100 + 500 + 1000;
+      printf("%i, %i: %s \n", i, k, linha); //+100 + 500 + 1000 pois ja tem 1600 registros no struct
+      strcpy(o[k].cod, strtok(linha, "\n")); 
+      o[k].mes = 4;
+      j++;
+    }
+    i++;
+  }
+  printf("Fim do quarto arquivo.\n");
+  
+  i = 0;
+  j = 0;
+  while (fgets(linha, 11, mes5)){
+    if (i % 2 == 0){
+      k = j + 100 + 500 + 1000 + 5000;
+      printf("%i, %i: %s \n", i, k, linha); //ja tem 6600 registros no struct
+      strcpy(o[k].cod, strtok(linha, "\n")); 
+      o[k].mes = 5;
+      j++;
+    }
+    i++;
+  }
+  printf("Fim do quinto arquivo.\n");
+  
+  /* codigo pra imprimir os registros
+  i = 0;
+  while (i < 16600) {
     printf("%i : %s\n", i, o[i].cod);
     i++;
   }
-
+  */
 }
